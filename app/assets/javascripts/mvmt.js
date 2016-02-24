45c62cd1b772c3f11c9a607f6f1ab7782c909e0b
Felix Ng <felix.ng@euromoneyasia.com>
2016-02-24 10:55:51 +0000

    angular with turbolink test 2

--- app/assets/javascripts/mvmt.js
+++ app/assets/javascripts/mvmt.js
@@ -1,22 +1,24 @@
 var app = angular.module('mvmt', []);
 
-$(document).on('ready page:load', ->
-  angular.bootstrap(document, ['mvmt'])
-)
-//
-//app.controller('CategoryController', function() {
-//    var catMenu = this;
-//
-//    catMenu.filters = { };
-//    console.log('in');
-//    catMenu.addToFilter = function(category){
-//        console.log('in');
-//        var index = filters.indexOf(category)
-//        if (index > -1){
-//            filters.splice(index, 1, category);
-//        }
-//        else {
-//            filters.spice(index, 0, category);
-//        }
-//    }
-//});
+// This will cause your app to compile when Turbolinks loads a new page
+// and removes the need for ng-app in the DOM
+$(document).on('ready page:load', function(arguments) {
+  angular.bootstrap(document.body, ['mvmt'])
+});
+
+app.controller('CategoryController', function() {
+   var catMenu = this;
+
+   catMenu.filters = { };
+   console.log('in');
+   catMenu.addToFilter = function(category){
+       console.log('in');
+       var index = filters.indexOf(category)
+       if (index > -1){
+           filters.splice(index, 1, category);
+       }
+       else {
+           filters.spice(index, 0, category);
+       }
+   }
+});
