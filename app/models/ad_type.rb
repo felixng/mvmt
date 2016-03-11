1a898e9ed3258fdc1958447eac6654f239fc5725
felix.ng <nfelix@gmail.com>
2016-03-11 23:17:02 +0000

    fixed with sql

--- app/models/ad_type.rb
+++ app/models/ad_type.rb
@@ -2,15 +2,15 @@ class AdType < ActiveRecord::Base
   has_many :affiliates
 
   def self.getHorizontalTypes
-    return AdType.where( :width <= :height )
+    return AdType.where( "width > height" )
   end
 
   def self.getVerticalTypes
-    return AdType.where( :height >= :width )
+    return AdType.where( "width <= height" )
   end
 
   def self.getSquare
-    return AdType.where( (:height - :width).abs >= 150 )
+    return AdType.where( "abs(width - height) <= 150" )
   end
 
 end
