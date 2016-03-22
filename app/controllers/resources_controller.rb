4bf7186bf5908fef42ce75e6e052c40b537c28d9
felix.ng <nfelix@gmail.com>
2016-03-22 18:17:16 +0000

    single page re-style

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -28,6 +28,19 @@ class ResourcesController < ApplicationController
   end
 
   def show
+    set_meta_tags title: @resource.name + '- MVMT - A curated list of studios and schools for people who move - Movement, Gymnastics, Circus, Parkour, Free Running, Dance, Crossfit'
+    set_meta_tags description: 'Discover yourself through movement.'
+    set_meta_tags keywords: %w[London Movement, London Gymnastics, London Circus, London Parkour, London Free Running, London Dance, London Crossfit]
+    set_meta_tags canonical: @resources_url
+
+    set_meta_tags og: {
+                      sitename: 'MVMT',
+                      title:    'MVMT - A curated list of studios and schools for people who move - Movement, Gymnastics, Circus, Parkour, Free Running, Dance, Crossfit',
+                      description: 'Discover yourself through movement.',
+                      locale: 'en_GB',
+                      type: 'website',
+                      url:      'https://mvmt.io',
+                  }
     @resource
   end
 
