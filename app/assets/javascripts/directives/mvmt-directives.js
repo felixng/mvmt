d134c79ab1ee573c1e4b5363f66ad2b15159b965
felix.ng <nfelix@gmail.com>
2016-04-03 17:54:01 +0100

    js fix test

--- app/assets/javascripts/directives/mvmt-directives.js
+++ app/assets/javascripts/directives/mvmt-directives.js
@@ -1,12 +1,15 @@
-app.directive('onFinishRender', function ($timeout) {
+app.directive('onFinishRender', ['$timeout', '$parse', function ($timeout, $parse) {
         return {
             restrict: 'A',
             link: function (scope, element, attr) {
                 if (scope.$last === true) {
                     $timeout(function () {
                         scope.$emit('ngRepeatFinished');
+                        if(!!attr.onFinishRender){
+                            $parse(attr.onFinishRender)(scope);
+                        }
                     });
                 }
             }
         }
-});
+}]);
